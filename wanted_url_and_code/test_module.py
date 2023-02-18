0f5b77c8a2977782604b8e6e5c5c531678648b43
bulatshuh <Bulat.Khisamutdinov@icl-services.com>
2023-02-18 18:22:48 +0300

    edit assersion message

--- wanted_url_and_code/test_module.py
+++ wanted_url_and_code/test_module.py
@@ -4,4 +4,5 @@ import requests
 def test_given_url(get_url, get_status_code):
     response = requests.get(get_url)
 
-    assert response.status_code == int(get_status_code), 'ERROR !!!'
+    assert response.status_code == int(get_status_code), f'Wrong status code, {int(get_status_code)} ' \
+                                                         f'is expected'
